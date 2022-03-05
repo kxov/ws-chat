d9c8d3b38c3e9cc080254aef97384a46b43f9e6d
makx <dev.maksim.t@alldigitalads.com>
2022-03-05 17:19:08 +0200

    add http server

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import { readFileSync } from 'fs';
+import { createServer } from 'http';
+
+const index = readFileSync('./index.html', 'utf8');
 
 const port = 6663;
+const server = createServer((req, res) => {
+    res.writeHead(200);
+    res.end(index);
+});
 
-const wss = new WebSocketServer({ port });
+server.listen(port, () => {
+    console.log(`Listen port ${port}`);
+});
+
+const wss = new WebSocketServer({ server });
 
 const clients = new Set();
 
