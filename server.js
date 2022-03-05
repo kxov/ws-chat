6778ff736a46f7e8913e0e0387b77c6d4041fbff
makx <dev.maksim.t@alldigitalads.com>
2022-03-05 13:22:39 +0200

    refac

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const port = 6663;
 
 const wss = new WebSocketServer({ port });
 
+const clients = new Set();
+
 wss.on('connection', (ws, req) => {
 
-  ws.on('message', (data, isBinary) => {
+    clients.add(ws);
 
-    for (const client of wss.clients) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data, { binary: isBinary });
+    ws.on('message', (data, isBinary) => {
+
+        for (const client of wss.clients) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
         }
-    }
-  });
+    });
 
-  console.log(`new connection from ${req.socket.remoteAddress}!`);
 
+    const ip = req.socket.remoteAddress;
+
+    console.log(`new connection from ${ip}!`);
 });
