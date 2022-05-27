bfca93a2066b795d5a4235b2394899d3a2813e10
makx <dev.maksim.t@alldigitalads.com>
2022-05-27 23:00:07 +0300

    lint & prettier

--- server.js
+++ server.js
@@ -6,36 +6,36 @@ const index = readFileSync('./index.html', 'utf8');
 
 const port = 6663;
 const server = createServer((req, res) => {
-    res.writeHead(200);
-    res.end(index);
+  res.writeHead(200);
+  res.end(index);
 });
 
 server.listen(port, () => {
-    console.log(`Listen port ${port}`);
+  console.log(`Listen port ${port}`);
 });
 
 const wss = new WebSocketServer({ server });
 
 const clients = new Set();
-const messages = [];
+//const messages = [];
 
 wss.on('connection', (ws, req) => {
-    const ip = req.socket.remoteAddress;
-    console.log(`New connection from ${ip}!`);
+  const ip = req.socket.remoteAddress;
+  console.log(`New connection from ${ip}!`);
 
-    clients.add(ws);
+  clients.add(ws);
 
-    ws.on('message', (data, isBinary) => {
-        for (const client of wss.clients) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: isBinary });
-            }
-        }
-    });
+  ws.on('message', (data, isBinary) => {
+    for (const client of wss.clients) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    }
+  });
 
-    ws.on('close', () => {
-        console.log(`Disconnected ${ip}`);
+  ws.on('close', () => {
+    console.log(`Disconnected ${ip}`);
 
-        clients.delete(ws);
-    });
+    clients.delete(ws);
+  });
 });
